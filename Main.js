76f94369e161d31bebad480d372f46609ee23cf1
caoyong <caoyong@lvmama.com>
2019-03-04 08:28:31 +0800

    initial commit

---
+++ Main.js
@@ -0,0 +1,35 @@
+import {ResourceLoader} from "./js/base/ResourceLoader.js";
+import {BackGround} from "./js/runtime/BackGround.js";
+import {DataStore} from "./js/base/DataStore.js";
+import {Director} from "./js/Director.js";
+import {Land} from "./js/runtime/Land.js";
+import {Birds} from "./js/player/Birds.js";
+
+export class Main {
+    constructor() {
+        this.canvas = document.getElementById("game_canvas");
+        this.ctx = this.canvas.getContext("2d");
+        this.dataStore = DataStore.getInstance();
+        this.director = Director.getInstance();
+        const loader = ResourceLoader.create();
+        loader.onLoaded(map => this.onResourceFirstLoaded(map));
+    }
+
+    onResourceFirstLoaded(map) {
+        this.dataStore.ctx = this.ctx;
+        this.dataStore.res = map;
+        this.init();
+
+    }
+
+    init() {
+        this.director.isGameOver = false;
+        this.dataStore
+            .put('pencils', [])
+            .put('background', BackGround)
+            .put('land', Land)
+            .put('birds', Birds);
+        this.director.createPencil();
+        this.director.run();
+    }
+}
---
+++ js/player/Birds.js
@@ -0,0 +1,9 @@
+import {Sprite} from "../base/Sprite.js";
+
+export class Birds extends Sprite {
+    constructor() {
+        const image = Sprite.getImage('birds');
+        super(image, 0, 0, image.width, image.height,
+            0, 0, image.width, image.height);
+    }
+}
